1e41a212be70012457b9275a6b2fe216cea5222b
ivangol739 <ivangol739@gmail.com>
2025-01-26 22:40:21 +0300

    add pred

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+#менеджер, который позволит нам получать опубликованные посты через Post.published.all()
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -16,6 +21,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True) #поле даты созданияэ
     updated = models.DateTimeField(auto_now=True) #поле даты изменения
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # пользовательский менеджер
 
     class Meta:
         ordering = ['-publish']
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post_detail.html', {'post': post})
