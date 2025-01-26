cd2ab91e53705b448ff5a24c7519e863dca93035
ivangol739 <ivangol739@gmail.com>
2025-01-26 21:13:56 +0300

    add admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author'] #боковая модель с фильтрацией
+    search_fields = ['title', 'body'] #Строка поиска
+    prepopulated_fields = {'slug': ('title',)} #Автоматическое заполнение
+    raw_id_fields = ['author'] #Поиск при выборе автора
+    date_hierarchy = 'publish' #Навигационные ссылки, предназначенные для перемещения по иерархии дат, ниже поиска
+    ordering = ['status', 'publish'] #упорядочивание по статусу
