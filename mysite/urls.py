f007637c1f4cb2d85aa0f1e5ba755322b7b7a5a4
ivangol739 <ivangol739@gmail.com>
2025-02-06 22:40:00 +0300

    добавлена аутентификация

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
